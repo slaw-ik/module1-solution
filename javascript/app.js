8ec8615c78bee2b34580284dbaf53fc9794fe756
myroslav <myroslav.nastenko@gmail.com>
2016-11-14 16:13:10 +0100

    assignment1 code

--- javascript/app.js
+++ javascript/app.js
@@ -1,63 +1,49 @@
 (function () {
-    "use strict";
+    'use strict';
 
-    angular.module("ShoppingListCheckOff", [])
-        .controller("ToBuyController", ToBuyController)
-        .controller("AlreadyBoughtController", AlreadyBoughtController)
-        .service("CheckOffService", ShoppingListCheckOffService);
+    angular.module('LunchCheck', [])
+        .controller('LunchCheckController', LunchCheckController);
 
-    ToBuyController.$inject = ['CheckOffService'];
-    function ToBuyController(CheckOffService) {
-        var toBuy = this;
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController($scope) {
+        $scope.msg = "";
+        $scope.textClass = "";
+        $scope.borderClass = "";
+        $scope.showMessage = false;
 
-        toBuy.items = CheckOffService.getToBuyItems();
+        $scope.check = function () {
+            var removeEmptyStrings = function (n) {
+                    return n && n.replace(/\s/g, "").length > 0
+                },
+                calculterMessage = function (count) {
+                    if (count > 3) {
+                        return "Too much!";
+                    } else {
+                        return "Enjoy!";
+                    }
+                }
 
-        toBuy.buyItem = function (index) {
-            CheckOffService.buyItem(index);
+            if ($scope.menu) {
+                var foodsAray = $scope.menu.split(','),
+                    allCount = foodsAray.length,
+                    nonEmptyCount = foodsAray.filter(removeEmptyStrings).length;
+
+                $scope.msg = calculterMessage(nonEmptyCount);
+
+                if (allCount != nonEmptyCount) {
+                    $scope.showMessage = true;
+                    $scope.borderClass = "red";
+                } else {
+                    $scope.borderClass = "green";
+                    $scope.showMessage = false;
+                }
+                $scope.textClass = "green";
+
+            } else {
+                $scope.msg = "Please enter data first";
+                $scope.textClass = "red";
+                $scope.borderClass = "red";
+            }
         }
-    }
-
-    AlreadyBoughtController.$inject = ['CheckOffService'];
-    function AlreadyBoughtController(CheckOffService) {
-        var alreadyBought = this;
-
-        alreadyBought.items = CheckOffService.getAlreadyBoughtItems();
-    }
-
-    function ShoppingListCheckOffService() {
-        var service = this,
-            toBuyItems = [{
-                name: 'item1',
-                quantity: 10
-            }, {
-                name: 'item2',
-                quantity: 20
-            }, {
-                name: 'item3',
-                quantity: 30
-            }, {
-                name: 'item4',
-                quantity: 40
-            }],
-            alreadyBoughtItems = [];
-
-        service.buyItem = function (index) {
-            var item = toBuyItems[index];
-            alreadyBoughtItems.push(item);
-            toBuyItems.splice(index, 1);
-            console.log(index);
-            console.log(toBuyItems);
-            console.log(alreadyBoughtItems);
-        };
-
-        service.getToBuyItems = function () {
-            return toBuyItems;
-        };
-
-        service.getAlreadyBoughtItems = function () {
-            return alreadyBoughtItems;
-        }
-
-
     }
 })();
