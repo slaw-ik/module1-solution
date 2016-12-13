bde7068ed9deb0c500f6fc9cc6dba16603dcb6fe
Myroslav Nastenko <nastenko@essential-data.sk>
2016-12-13 22:37:47 +0100

    module 3 solution

--- javascript/app.js
+++ javascript/app.js
@@ -1,49 +1,77 @@
 (function () {
     'use strict';
 
-    angular.module('LunchCheck', [])
-        .controller('LunchCheckController', LunchCheckController);
+    angular.module('NarrowItDownApp', [])
+        .controller('NarrowItDownController', NarrowItDownController)
+        .service('MenuSearchService', MenuSearchService)
+        .directive('foundItems', FoundItems)
+        .constant('ApiBasePath', "http://davids-restaurant.herokuapp.com");
 
-    LunchCheckController.$inject = ['$scope'];
-    function LunchCheckController($scope) {
-        $scope.msg = "";
-        $scope.textClass = "";
-        $scope.borderClass = "";
-        $scope.showMessage = false;
 
-        $scope.check = function () {
-            var removeEmptyStrings = function (n) {
-                    return n && n.replace(/\s/g, "").length > 0
-                },
-                calculterMessage = function (count) {
-                    if (count > 3) {
-                        return "Too much!";
-                    } else {
-                        return "Enjoy!";
-                    }
-                }
+    function FoundItems() {
+        var ddo = {
+            restrict: 'E',
+            templateUrl: 'found_items.html',
+            scope: {
+                foundItems: '<',
+                onRemove: '&'
+            },
+            controller: FoundItemsDirectiveController,
+            controllerAs: 'foundCtrl',
+            bindToController: true
+        };
 
-            if ($scope.menu) {
-                var foodsAray = $scope.menu.split(','),
-                    allCount = foodsAray.length,
-                    nonEmptyCount = foodsAray.filter(removeEmptyStrings).length;
+        return ddo;
+    }
 
-                $scope.msg = calculterMessage(nonEmptyCount);
+    function FoundItemsDirectiveController() {
+        var foundCtrl = this;
 
-                if (allCount != nonEmptyCount) {
-                    $scope.showMessage = true;
-                    $scope.borderClass = "red";
-                } else {
-                    $scope.borderClass = "green";
-                    $scope.showMessage = false;
-                }
-                $scope.textClass = "green";
-
-            } else {
-                $scope.msg = "Please enter data first";
-                $scope.textClass = "red";
-                $scope.borderClass = "red";
-            }
+        foundCtrl.noItems = function () {
+            return foundCtrl.foundItems.length == 0;
         }
     }
+
+    NarrowItDownController.$inject = ['MenuSearchService'];
+    function NarrowItDownController(MenuSearchService) {
+        var ctrl = this;
+
+        ctrl.found = [];
+
+        ctrl.getMatchedMenuItems = function (searchTerm) {
+            var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
+
+            promise.then(function (found) {
+                ctrl.found = found;
+            })
+        };
+
+        ctrl.removeItem = function (index) {
+            console.log('Remove Item' + index);
+            ctrl.found.splice(index, 1);
+        };
+    }
+
+    MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+    function MenuSearchService($http, ApiBasePath) {
+        var service = this;
+
+        service.getMatchedMenuItems = function (searchTerm) {
+            return $http({
+                url: (ApiBasePath + '/menu_items.json')
+            })
+                .then(function (result) {
+
+                    var menuItems = result.data.menu_items,
+                        foundItems;
+
+                    foundItems = menuItems.filter(function (elem) {
+                        return searchTerm && (searchTerm.trim() != "") && (elem.name.toLowerCase().indexOf(searchTerm) > -1)
+                    });
+
+                    return foundItems;
+                });
+        }
+    }
+
 })();
